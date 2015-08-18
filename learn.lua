9856ed76fe09b44e0315150faddb191244145988
antoine-cha <antoine@moodstocks.com>
2015-08-18 16:54:09 +0200

    fix and cuda support

--- learn.lua
+++ learn.lua
@@ -1,146 +1,178 @@
 require 'nn'
 require 'optim'
-
+require 'cunn'
 
 -- define the dataset
-function getDataset(file, yesNo)
+local function getDataset(file, only2classes)
+  -- Get the dataset object from the file.
+  -- Format it if only 2 classes (change to 1, -1)
+  -- Return a Tensor 
+  -- -------------------------
+  -- file (string)
+  --  relative path to the t7 file 
+  -- only2classes(bool)
+  --  specify whether the dataset only contains 2 classes
 
-    dataset = torch.load(file)
-    -- Modify dataset to get Tensors as classes
-    if yesNo then
-        for i, c in ipairs(dataset) do
-            if dataset[i][2] == 2 then
-                dataset[i][2] = torch.Tensor(1)
-                dataset[i][2][1] = -1
-            else
-                dataset[i][2] = torch.Tensor(1)
-                dataset[i][2][1] = 1
-            end
-        end
+  local dataset = torch.load(file)
+  -- Modify dataset to get Tensors as classes
+  if only2classes then
+    for i, c in ipairs(dataset) do
+      if dataset[i][2] == 2 then
+        dataset[i][2] = torch.Tensor(1)
+        dataset[i][2][1] = -1
+      else
+        dataset[i][2] = torch.Tensor(1)
+        dataset[i][2][1] = 1
+      end
     end
-    -- Needed by the algorithm
-    function dataset:size() return table.getn(dataset) end
-    return dataset
+  end
+  -- Needed by the algorithm
+  function dataset:size() return #table[dataset] end
+  return dataset
 end
 
 
--- define the network
-function createNetwork()
-    -- For testing purpose
-    local x = torch.Tensor(3,32,32)
+local function createNetwork(nb_classes)
+  -- Define the network as in the paper
+  local nb_classes = nb_classes or 43
+  -- Enables to test the output sizes
+  local x = torch.Tensor(3,32,32)
 
-    local mlp = nn.Sequential()
-    kW = 3; kH=3;
-    local conv1 = nn.SpatialConvolutionMM(3, 108, kW, kH)
-    local subnor1 = nn.SpatialSubtractiveNormalization(108)
-    local divnor1 = nn.SpatialDivisiveNormalization(108)
-    local max1 = nn.SpatialMaxPooling(4, 4, 4, 4)
-    -- Branching now
-    local ways = nn.Concat(1)
-    -- First branch with 2 layers of convolutions
-    local way1 = nn.Sequential()
-    local conv2 = nn.SpatialConvolutionMM(108, 108, kW, kH)
-    local subnor2 = nn.SpatialSubtractiveNormalization(108)
-    local divnor2 = nn.SpatialDivisiveNormalization(108)
-    local max2 = nn.SpatialMaxPooling(4, 4, 4, 4)
-    local conv3 = nn.SpatialConvolutionMM(108, 50, 1, 1) 
-    way1:add(conv2)
-    way1:add(nn.ReLU())
-    way1:add(subnor2)
-    way1:add(divnor2)
-    way1:add(max2)
-    way1:add(conv3)
-    way1:add(nn.ReLU())
+  local mlp = nn.Sequential()
+  kW = 3; kH=3;
+  local conv1 = nn.SpatialConvolutionMM(3, 108, kW, kH)
+  local subnor1 = nn.SpatialSubtractiveNormalization(108)
+  local divnor1 = nn.SpatialDivisiveNormalization(108)
+  local max1 = nn.SpatialMaxPooling(4, 4, 4, 4)
+  -- Branching now
+  local ways = nn.Concat(1)
+  -- First branch with 2 layers of convolutions
+  local way1 = nn.Sequential()
+  local conv2 = nn.SpatialConvolutionMM(108, 108, kW, kH)
+  local subnor2 = nn.SpatialSubtractiveNormalization(108)
+  local divnor2 = nn.SpatialDivisiveNormalization(108)
+  local max2 = nn.SpatialMaxPooling(4, 4, 4, 4)
+  local conv3 = nn.SpatialConvolutionMM(108, 50, 1, 1) 
+  way1:add(conv2)
+  way1:add(nn.ReLU())
+  way1:add(subnor2)
+  way1:add(divnor2)
+  way1:add(max2)
+  way1:add(conv3)
+  way1:add(nn.ReLU())
 
-    local way2 = nn.Sequential()
-    way2:add(nn.SpatialConvolution(108,50,7,7))
-    way2:add(nn.Reshape(50,1)) 
-    ways:add(way1)
-    ways:add(way2)
+  local way2 = nn.Sequential()
+  way2:add(nn.SpatialConvolution(108,50,7,7))
+  way2:add(nn.Reshape(50,1)) 
+  ways:add(way1)
+  ways:add(way2)
 
-    --Equivalent to a fully connected one
-    --This removes the need for a reshape
 
-    mlp:add(conv1)
-    mlp:add(nn.ReLU())
-    mlp:add(subnor1)
-    mlp:add(divnor1)
-    mlp:add(max1)
-    -- Branching 
-    mlp:add(ways)
+  mlp:add(conv1)
+  mlp:add(nn.ReLU())
+  mlp:add(subnor1)
+  mlp:add(divnor1)
+  mlp:add(max1)
+  -- Branching 
+  mlp:add(ways)
 
-    mlp:add(nn.Reshape(100))
+  mlp:add(nn.Reshape(100))
 
-    mlp:add(nn.Linear(100,3))
-    mlp:add(nn.SoftMax())
-    print(mlp:forward(x):size())
-
-    return mlp
+  mlp:add(nn.Linear(100,nb_classes))
+  mlp:add(nn.SoftMax())
+  print('Network outputs :')
+  print(mlp:forward(x):size())
+  
+  return mlp
 end
 
-local yesNo = false
-dataset = getDataset('1k3classes.t7', yesNo)
-mlp = createNetwork()
----- Training Criterion
-if yesNo then
+
+
+local function trainNetwork(network, dataset_file, params)
+  -- Train a network given as input, with the given dataset and batchSize
+  -- --------------------------
+  -- Returns nothing :
+  --  the network object is changed by the function
+  -- --------------------------
+  -- network (mlp)
+  --  network to be trained
+  -- dataset_file (path)
+  --  relative path to the dataset file
+  -- only2classes(bool)
+  --  specifies the task to format the dataset
+  -- batchSize(int)
+  local dataset = getDataset(dataset_file, params.only2classes)
+  local cudaOn = params.cudaOn
+
+  if params.cudaOn then
+    network:cuda()
+  end
+  ---- Training Criterion defined by the task
+  if params.only2classes then
     criterion = nn.MSECriterion()
-else
+    print('MSE Criterion')
+  else
     criterion = nn.CrossEntropyCriterion()
-end
+    if cudaOn then
+      criterion:cuda()
+    end
+    print('Cross-entropy Criterion')
+  end
 
-stoc = false
-if stoc then
-    trainer = nn.StochasticGradient(mlp, criterion)
+  if params.batchSize == 1 then
+    local trainer = nn.StochasticGradient(network, criterion)
     trainer.learningRate = 0.01
     trainer.shuffleIndices = false
+    print('Training using normal stochastic gradient')
     trainer:train(dataset)
-else
-    f, df = mlp:getParameters()
-    print(f:size())
+  else
+    f, df = network:getParameters()
     -- Define the function to be optimized for SGD
-    --
+    _nidx_ = 1209 -- 9*128
     feval = function(f_new)
-        -- set x to x_new, if differnt
-        -- (in this simple example, x_new will typically always point to x,
-        -- so the copy is really useless)
-        if f ~= f_new then
-            f:copy(f_new)
-        end
-        -- select a new training sample
-        _nidx_ = (_nidx_ or 0) + 1
-        if _nidx_ > (#dataset) then _nidx_ = 1 end
-        local sample = dataset[_nidx_]
-        local target = sample[2]
-        local inputs = sample[1]
-        -- reset gradients (gradients are always accumulated, to accomodate 
-        -- batch methods)
-        df:zero()
-        --evaluate the loss function and its derivative wrt x,
-        local loss_x = criterion:forward(mlp:forward(inputs), target)
-        mlp:backward(inputs, criterion:backward(mlp.output, target))
-        -- return loss(x) and dloss/dx
-        return loss_x, df
+      -- set x to x_new, if different
+      -- (in this simple example, x_new will typically always point to x,
+      -- so the copy is really useless)
+      if f ~= f_new then
+        f:copy(f_new)
+      end
+      -- select a new training sample
+      _nidx_ = (_nidx_ or 0) + 1
+      if _nidx_ > (#dataset) then _nidx_ = 1 end
+      local sample = dataset[_nidx_]
+      local target = sample[2]
+      local inputs = sample[1]
+      if cudaOn then
+        inputs:cuda()
+        --target:cuda()
+      end
+      
+
+      -- reset gradients (gradients are always accumulated, to accomodate 
+      -- batch methods)
+      df:zero()
+      --evaluate the loss function and its derivative wrt x,
+      local loss_x = criterion:forward(network:forward(inputs), target)
+      network:backward(inputs, criterion:backward(network.output, target))
+      -- return loss(x) and dloss/dx
+      return loss_x, df
     end
 
-    sgd_params = {
-        learningRate = 1e-3,
-        learningRateDecay = 1e-4,
-        weightDecay = 0,
-        momentum = 0
-        }
+
     -- Start training
-    for i=1,1e4 do
-        current_loss = 0
-        for i= 1,128 do
-            _, fs = optim.sgd(feval, f, sgd_params)
-            current_loss = current_loss + fs[1]
-        end
-        current_loss = current_loss / 100
-        print(i ..'th iteration, current_loss :' .. current_loss)
+    for i=1, params.nb_epochs do
+      current_loss = 0
+      for i= 1,params.batchSize do
+        _, fs = optim.sgd(feval, f, params)
+        current_loss = current_loss + fs[1]
+      end
+      current_loss = current_loss / 100
+      print(i ..'th iteration, current_loss :' .. current_loss)
     end
+  end
 end
 
-
-
-
-
+return {getDataset = getDataset,
+        createNetwork = createNetwork,
+        trainNetwork = trainNetwork
+      }
