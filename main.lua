88562c843ce24d1f8972d21e4a1605ef8aaed971
antoine-cha <antoine@moodstocks.com>
2015-08-20 18:18:26 +0200

    command line calls and save parameters

--- main.lua
+++ main.lua
@@ -3,28 +3,36 @@ local learn = require 'learn'
 local path = require 'pl.path'
 local dir = require 'pl.dir'
 
--- training parameters
-local modelDir = './models/'
-local filename = 'model-.t7'
-local dataPath = './dataset-43c-allex-2fa.t7'
+local args = lapp [[
+    Gets the data in the right format
+    -r, --lr (number) learning rate
+    -m, --momentum (number)
+    --lrdecay  (number) learning rate decay
+    --weightdecay (number) weight decay
+    --model (string) model name
+]]
 local params = {
-      filename = path.join(modelDir, filename),
-      batchSize = 100,
+      orig_file = '',
+      dataPath = './dataset-43c-allex.t7',
+      modelDir = './models/',
+      filename = args.model or 'model.t7',
       only2classes = false,
       cudaOn = true,
-      nb_epochs = 10000,
+      nb_epochs = 10,
       freq_save = 500,
       -- SGD parameters
-      learningRate = 1e-1,
-      learningRateDecay = 1e-4,
-      weightDecay = 0.0005,
-      momentum = 0.1,
-      dampening = 0.0,
+      batchSize = 100,
+      learningRate = args.lr or 1,
+      learningRateDecay = args.lrdecay or 1e-4,
+      weightDecay = args.weightdecay or 0,
+      momentum = args.momentum or 0.5,
+      dampening = 0,
       nesterov = 1,
 }
 
-if not path.isdir(modelDir) then
-  dir.makepath(modelDir)
+params.filename = path.join(params.modelDir, params.filename)
+if not path.isdir(params.modelDir) then
+  dir.makepath(params.modelDir)
 end
 
 if path.exists(params.filename) then
@@ -35,16 +43,20 @@ if path.exists(params.filename) then
     n = n + 1
   end
   params.filename = (name .. '-' .. '%.2i' .. ext):format(n)
+  params.paramsFile = (name .. '-' .. '%.2i' .. params .. ext):format(n)
 end
 
-
-local network = learn.createNetwork() 
-
 local mlp_ = nn.Sequential()
-mlp_:add(network)
+if params.orig_file ~= '' then
+  local network = torch.load(params.orig_file)
+  print('Using ' .. params.orig_file .. ' as starting network')
+  mlp_:add(network)
+else
+  local network = learn.createNetwork() 
+  mlp_:add(network)
+end
 
 print(params)
---print(sgd_paramslearn.trainNetwork(mlp_, dataPath, train_params)
-learn.trainNetwork(mlp_, dataPath, params)
-
+learn.trainNetwork(mlp_, params)
+torch.save(params.paramsFile,params)
 torch.save(params.filename, mlp_)
