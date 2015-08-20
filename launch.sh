ba204fc347820935b175a2a1691dd9c3b486c4c7
antoine-cha <antoine@moodstocks.com>
2015-08-20 19:22:42 +0200

    launched experiences

---
+++ launch.sh
@@ -0,0 +1,18 @@
+# Use to launch different exp during one night
+th main.lua --lr 0.1 --m 0.3 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.1 --m 0.4 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.1 --m 0.5 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.1 --m 0.6 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.1 --m 0.7 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.2 --m 0.3 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.2 --m 0.4 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.2 --m 0.5 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.2 --m 0.6 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.2 --m 0.7 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.3 --m 0.4 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.3 --m 0.5 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.05 --m 0.4 --lrdecay 0.00001 --weightdecay 0.0001
+th main.lua --lr 0.05 --m 0.5 --lrdecay 0.00001 --weightdecay 0.0001
+
+
+
