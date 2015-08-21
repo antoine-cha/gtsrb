415542e641bff8a5ca82d05a996cad1e91d750fc
antoine-cha <antoine@moodstocks.com>
2015-08-21 19:44:44 +0200

    new experiments setups

--- launch.sh
+++ launch.sh
@@ -1,18 +1,18 @@
 # Use to launch different exp during one night
-th main.lua --lr 0.1 --m 0.3 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.1 --m 0.4 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.1 --m 0.5 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.1 --m 0.6 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.1 --m 0.7 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.2 --m 0.3 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.2 --m 0.4 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.2 --m 0.5 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.2 --m 0.6 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.2 --m 0.7 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.3 --m 0.4 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.3 --m 0.5 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.05 --m 0.4 --lrdecay 0.00001 --weightdecay 0.0001
-th main.lua --lr 0.05 --m 0.5 --lrdecay 0.00001 --weightdecay 0.0001
+th main.lua --lr 0.01 --m 0.3 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.01 --m 0.4 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.01 --m 0.5 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.01 --m 0.6 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.01 --m 0.7 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.02 --m 0.3 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.02 --m 0.4 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.02 --m 0.5 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.02 --m 0.6 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.02 --m 0.7 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.03 --m 0.4 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.03 --m 0.5 --lrdecay 0.0001 --weightdecay 0.0005
+th main.lua --lr 0.005 --m 0.4 --lrdecay 0.00001 --weightdecay 0.0001
+th main.lua --lr 0.005 --m 0.5 --lrdecay 0.00001 --weightdecay 0.0001
 
 
 
