b2466d0f99b324dce499d5884a05c1b498901fc1
antoine-cha <antoine@moodstocks.com>
2015-08-24 19:25:04 +0200

    new experience setup

--- launch.sh
+++ launch.sh
@@ -1,18 +1,10 @@
 # Use to launch different exp during one night
-th main.lua --lr 0.01 --m 0.3 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.01 --m 0.4 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.01 --m 0.5 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.01 --m 0.6 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.01 --m 0.7 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.02 --m 0.3 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.02 --m 0.4 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.02 --m 0.5 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.02 --m 0.6 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.02 --m 0.7 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.03 --m 0.4 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.03 --m 0.5 --lrdecay 0.0001 --weightdecay 0.0005
-th main.lua --lr 0.005 --m 0.4 --lrdecay 0.00001 --weightdecay 0.0001
-th main.lua --lr 0.005 --m 0.5 --lrdecay 0.00001 --weightdecay 0.0001
+th main.lua --lr 0.005 --m 0.9 --lrdecay 0.001 --weightdecay 0.005
+th main.lua --lr 0.005 --m 0.8 --lrdecay 0.001 --weightdecay 0.005
+th main.lua --lr 0.005 --m 0.8 --lrdecay 0.001 --weightdecay 0.001
+th main.lua --lr 0.001 --m 0.9 --lrdecay 0.001 --weightdecay 0.001
+th main.lua --lr 0.0005 --m 0.9 --lrdecay 0.001 --weightdecay 0.001
+th main.lua --lr 0.0005 --m 0.9 --lrdecay 0.0001 --weightdecay 0.001
 
 
 
